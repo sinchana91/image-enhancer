7043b43fd0e61ca1c20f3fa29c755d01c053ea9e
Sona-A-R <gowdasona2@gmail.com>
2024-03-03 12:00:25 +0530

    second day

---
+++ img-enhancer/app/(root)/transformation/[id]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TransformationPage = () => {
+  return (
+    <div>TransformationPage</div>
+  )
+}
+
+export default TransformationPage
+// localhost:3000/transformations
+// localhost:3000/transformation/[id]:Dynamic routes
