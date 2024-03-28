abb0da5e95efefb7b1a9e72517bace7215fea5b4
sinchana c poojary <sinchanac91@gmail.com>
2024-03-28 20:25:58 +0530

    some minute changes

--- app/(root)/transformation/add/[type]/page.tsx
+++ app/(root)/transformation/add/[type]/page.tsx
@@ -9,7 +9,7 @@ const AddTransformationTypePage = async ({ params: { type } }: SearchParamProps)
   const { userId } = auth();
   const transformation = transformationTypes[type];
 
-  if(!userId) redirect('/sign-in')
+  if(!userId) return
 
   const user = await getUserById(userId);
 
